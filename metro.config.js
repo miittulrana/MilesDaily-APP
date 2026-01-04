c66d9ef83eee9f71c9023b8b7e9792ea418aefc8
miittulrana <miittulrana@gmail.com>
2026-01-04 21:59:13 +0100

    DSRT

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,26 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
+config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json', 'cjs'];
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.startsWith('@supabase/')) {
+    const packageName = moduleName.split('/')[1].split('/')[0]; // Get package name only
+    const resolvedPath = `${context.projectRoot}/node_modules/@supabase/${packageName}/dist/index.cjs`;
+    
+    try {
+      const fs = require('fs');
+      if (fs.existsSync(resolvedPath)) {
+        return {
+          filePath: resolvedPath,
+          type: 'sourceFile',
+        };
+      }
+    } catch (e) {
+    }
+  }
+  
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
